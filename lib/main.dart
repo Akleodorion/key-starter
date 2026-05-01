a23ea9620a968444ac303663e4a446a2a841ace9
christian bondzie <christian.zap@hotmail.fr>
2026-05-01 09:36:18 +0200

    Main dart update.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:key_starter/core/enums/clef_mode.dart';
-import 'package:key_starter/core/theme/app_colors.dart';
 import 'package:key_starter/core/theme/app_theme.dart';
-import 'package:key_starter/core/widgets/staff_widget.dart';
+import 'package:key_starter/features/session/presentation/pages/session_setup_page.dart';
 import 'package:key_starter/injection_container.dart';
 
 void main() async {
@@ -27,27 +25,7 @@ class KeyStarterApp extends StatelessWidget {
       title: 'Key Starter',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
-      home: const _Placeholder(),
-    );
-  }
-}
-
-class _Placeholder extends StatelessWidget {
-  const _Placeholder();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.ivory,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: StaffWidget(
-            diatonicStep: 4,
-            clef: ClefMode.treble,
-          ),
-        ),
-      ),
+      home: const SessionSetupPage(),
     );
   }
 }
